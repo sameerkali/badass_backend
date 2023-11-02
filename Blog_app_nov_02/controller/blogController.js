3cf7e3a77551cce2d400ac44ff28288c615fc419
Sameer Faridi <s.faridi007@gmail.com>
2023-11-02 21:01:16 +0530

    error solve in delete user's blog

--- Blog_app_nov_02/controller/blogController.js
+++ Blog_app_nov_02/controller/blogController.js
@@ -1,4 +1,6 @@
 const blogModel = require("../model/blogModel");
+const userModel = require("../model/userModel");
+const mongoose = require("mongoose");
 
 // Get all blogs                                         ...............................................
 exports.getAllBlogsController = async (req, res) => {
@@ -28,16 +30,30 @@ exports.getAllBlogsController = async (req, res) => {
 // create a new blog                                         ...............................................
 exports.createBlogController = async (req, res) => {
   try {
-    const { title, description, image } = req.body;
+    const { title, description, image, user } = req.body;
     //validation
-    if (!title || !description || !image) {
+    if (!title || !description || !image || !user) {
       return res.status(400).send({
         sucess: false,
         messege: "please provide all fields"
       });
     }
+    const existingUser = await userModel.findById(user);
+    // validation
+    if (!existingUser) {
+      return res.status(404).send({
+        success: false,
+        message: "Unable to find User"
+      });
+    }
     //save the blog
-    const newBlog = new blogModel({ title, description, image }); //main logic
+    const newBlog = new blogModel({ title, description, image, user }); //main logic
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newBlog.save({ session });
+    existingUser.blogs.push(newBlog);
+    await existingUser.save({ session });
+    await session.commitTransaction();
     await newBlog.save();
     return res.status(201).send({
       sucess: true,
@@ -82,7 +98,7 @@ exports.updateBlogController = async (req, res) => {
 exports.getBlogByIdController = async (req, res) => {
   try {
     const { id } = req.params;
-    const blog = await blogModel.findById(id);  //main logic
+    const blog = await blogModel.findById(id); //main logic
     if (!blog) {
       return res.status(404).send({
         success: false,
@@ -107,16 +123,29 @@ exports.getBlogByIdController = async (req, res) => {
 // delete a blog                                         ...............................................
 exports.deleteBlogController = async (req, res) => {
   try {
-    await blogModel.findByIdAndDelete(req.params.id) //findOneAndDelete //main logic
-    return res.status(200).send({
-        sucess: true,
-        messege: "Delete blog sucessfully"
+    const blog = await blogModel
+      .findByIdAndDelete(req.params.id)
+      .populate("user");
+
+    if (!blog) {
+      // If blog is null, it means no blog was found with the given ID
+      return res.status(404).send({
+        success: false,
+        message: "Blog not found"
       });
+    }
+
+    await blog.user.blogs.pull(blog);
+    await blog.user.save();
+    return res.status(200).send({
+      success: true,
+      message: "Delete blog successfully"
+    });
   } catch (error) {
     console.log(error);
     return res.status(400).send({
-      sucess: false,
-      messege: "Error while deleting the blog"
+      success: false,
+      message: "Error while deleting the blog"
     });
   }
 };
--- Blog_app_nov_02/model/userModel.js
+++ Blog_app_nov_02/model/userModel.js
@@ -1,20 +1,29 @@
 const mongoose = require("mongoose");
 
 //user schema
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "username is required"]
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"]
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"]
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"]
+    },
+    blogs: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Blog"
+      }
+    ]
   },
-  email: {
-    type: String,
-    required: [true, "email is required"]
-  },
-  password: {
-    type: String,
-    required: [true, "password is required"]
-  }
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 const userModel = new mongoose.model("user", userSchema);
 module.exports = userModel;
