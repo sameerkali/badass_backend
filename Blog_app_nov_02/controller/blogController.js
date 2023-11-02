22cdb9907123335c94cbbc5e654f90d46d9e68d7
Sameer Faridi <s.faridi007@gmail.com>
2023-11-02 20:11:07 +0530

    blog backend compleated CRUD

--- Blog_app_nov_02/controller/blogController.js
+++ Blog_app_nov_02/controller/blogController.js
@@ -1,9 +1,9 @@
 const blogModel = require("../model/blogModel");
 
-// Get all blogs
+// Get all blogs                                         ...............................................
 exports.getAllBlogsController = async (req, res) => {
   try {
-    const blogs = await blogModel.find({});
+    const blogs = await blogModel.find({}); //main logic
     if (!blogs) {
       return res.status(200).send({
         sucess: false,
@@ -11,11 +11,11 @@ exports.getAllBlogsController = async (req, res) => {
       });
     }
     return res.status(200).send({
-        sucess: true,
-        BlogCount: blogs.length,
-        messege: "All blog list",
-        blogs
-      });
+      sucess: true,
+      BlogCount: blogs.length,
+      messege: "All blog list",
+      blogs
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
@@ -25,25 +25,25 @@ exports.getAllBlogsController = async (req, res) => {
   }
 };
 
-// create a new blog
+// create a new blog                                         ...............................................
 exports.createBlogController = async (req, res) => {
   try {
-    const {title, description, image} = req.body
-    //validation 
-    if(!title || !description || !image){
-        return res.status(400).send({
-            sucess: false,
-            messege: "please provide all fields"
-          });
+    const { title, description, image } = req.body;
+    //validation
+    if (!title || !description || !image) {
+      return res.status(400).send({
+        sucess: false,
+        messege: "please provide all fields"
+      });
     }
     //save the blog
-    const newBlog = new blogModel({title, description, image})
-    await newBlog.save()
+    const newBlog = new blogModel({ title, description, image }); //main logic
+    await newBlog.save();
     return res.status(201).send({
-        sucess: true,
-        messege: "Blog created successfully",
-        newBlog
-      });
+      sucess: true,
+      messege: "Blog created successfully",
+      newBlog
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
@@ -53,38 +53,70 @@ exports.createBlogController = async (req, res) => {
   }
 };
 
-// Update a blog
+// Update a blog                                         ...............................................
 exports.updateBlogController = async (req, res) => {
   try {
+    const { id } = req.params;
+    const { title, description, image } = req.body;
+    const blog = await blogModel.findByIdAndUpdate(
+      /*main logic*/
+      id,
+      { ...req.body },
+      { new: true }
+    );
+    return res.status(200).send({
+      sucess: true,
+      messege: "Update the Blog successfully",
+      blog
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
       sucess: false,
-      messege: "Error while getting the blog's"
+      messege: "Error while updating the blog"
     });
   }
 };
 
-// get a blog by id
+// get single blog by id                                         ...............................................
 exports.getBlogByIdController = async (req, res) => {
   try {
+    const { id } = req.params;
+    const blog = await blogModel.findById(id);  //main logic
+    if (!blog) {
+      return res.status(404).send({
+        success: false,
+        message: "blog not found with this id"
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      message: "fetch single blog",
+      "meri taraf ka message": "mill gayaa bhai",
+      blog
+    });
   } catch (error) {
     console.log(error);
-    return res.status(500).send({
+    return res.status(400).send({
       sucess: false,
-      messege: "Error while getting the blog's"
+      messege: "Error while getting single blog"
     });
   }
 };
 
-// delete a blog
+// delete a blog                                         ...............................................
 exports.deleteBlogController = async (req, res) => {
   try {
+    await blogModel.findByIdAndDelete(req.params.id) //findOneAndDelete //main logic
+    return res.status(200).send({
+        sucess: true,
+        messege: "Delete blog sucessfully"
+      });
   } catch (error) {
     console.log(error);
-    return res.status(500).send({
+    return res.status(400).send({
       sucess: false,
-      messege: "Error while getting the blog's"
+      messege: "Error while deleting the blog"
     });
   }
 };
